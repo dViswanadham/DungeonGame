5f5f4c2a3f52e10d3c5759942e26a00db3913baa
Dheeraj Viswanadham <d.viswanadham@unsw.edu.au>
2019-10-24 15:10:10 +1100

    Added entity files

---
+++ src/unsw/dungeon/Enemy.java
@@ -0,0 +1,51 @@
+/**
+ * Team: Desktop_Support
+ * Members: Liam (z5207407) and Dheeraj (z5204820)
+ * 
+ * Started: 24/10/2019 | Last edited: 24/10/2019
+ * 
+ * Acknowledgement: some of the code may be similar to the lab code.
+ */
+
+
+package unsw.dungeon;
+
+/**
+ * The player entity
+ * @author Robert Clifton-Everest
+ *
+ */
+public class Enemy extends Entity {
+
+    private Dungeon dungeon;
+
+    /**
+     * Create an enemy positioned in square (x,y)
+     * @param x
+     * @param y
+     */
+    public Enemy(Dungeon dungeon, int x, int y) {
+        super(x, y);
+        this.dungeon = dungeon;
+    }
+
+    public void moveUp() {
+        if (getY() > 0)
+            y().set(getY() - 1);
+    }
+
+    public void moveDown() {
+        if (getY() < dungeon.getHeight() - 1)
+            y().set(getY() + 1);
+    }
+
+    public void moveLeft() {
+        if (getX() > 0)
+            x().set(getX() - 1);
+    }
+
+    public void moveRight() {
+        if (getX() < dungeon.getWidth() - 1)
+            x().set(getX() + 1);
+    }
+}
