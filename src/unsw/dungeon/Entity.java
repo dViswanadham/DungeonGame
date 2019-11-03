df16932181c82202b97222a9c0b419b02ec588e6
z5207407 <liam.eastwood@unsw.edu.au>
2019-11-03 16:43:20 +1100

    added observer

--- src/unsw/dungeon/Entity.java
+++ src/unsw/dungeon/Entity.java
@@ -1,5 +1,8 @@
 package unsw.dungeon;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleBooleanProperty;
@@ -11,12 +14,12 @@ import javafx.beans.property.SimpleIntegerProperty;
  * @author Robert Clifton-Everest
  *
  */
-public abstract class Entity {
+public abstract class Entity implements ObsInterface {
     // IntegerProperty is used so that changes to the entities position can be
     // externally observed. 
     private IntegerProperty x, y;
     private BooleanProperty seeable;
-    
+    private ArrayList<Constructor> observers;
     /**
      * Create an entity positioned in square (x,y)
      * 
@@ -27,35 +30,30 @@ public abstract class Entity {
         this.x = new SimpleIntegerProperty(x);
         this.y = new SimpleIntegerProperty(y);
         this.seeable = new SimpleBooleanProperty(true);
+        this.observers = new ArrayList<>();
     }
 
     public IntegerProperty x() {
-        
         return x;
     }
 
     public IntegerProperty y() {
-        
         return y;
     }
     
     public BooleanProperty seeable() {
-        
     	return seeable;
     }
     
     public void triggerSeeable(boolean transparency) {
-        
     	this.seeable.set(transparency);
     }
 
     public int getY() {
-        
         return y().get();
     }
 
     public int getX() {
-        
         return x().get();
     }
     
@@ -67,7 +65,6 @@ public abstract class Entity {
      * @return boolean
      */
     public boolean scanSquare(Token object) {
-        
         return false;
     }
     
@@ -79,7 +76,6 @@ public abstract class Entity {
      * @return boolean
      */
     public boolean scanSquare(Wall object) {
-        
         return false;
     }
     
@@ -91,7 +87,36 @@ public abstract class Entity {
      * @return boolean
      */
     public boolean scanSquare(Mobile object) {
-        
         return false;
     }
+    
+    public boolean collectObject(Dungeon dungeon, Inventory inventory) {
+    	inventory.addObject(this);
+    	dungeon.removeEntity(this);
+    	notifyObservers();
+    	return true;
+    }
+    
+	public boolean isBlocking() {
+		return false;
+	}
+    
+	@Override
+	public void registerObserver(Constructor c) {
+		if (!observers.contains(c)) {
+			observers.add(c);
+		}
+	}
+
+	@Override
+	public void removeObserver(Constructor o) {
+		observers.remove(o);
+	}
+
+	@Override
+	public void notifyObservers() {
+		for (Constructor c : observers) {
+			c.update(this);
+		}
+	}
 }
