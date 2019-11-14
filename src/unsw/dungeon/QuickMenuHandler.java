34c1d942f5b098e1ed99567409d2451d1f2482f7
Dheeraj Viswanadham <d.viswanadham@unsw.edu.au>
2019-11-14 12:51:15 +1100

    Added base front end code

---
+++ src/unsw/dungeon/QuickMenuHandler.java
@@ -0,0 +1,20 @@
+/**
+ * Team: Desktop_Support
+ * Members: Liam (z5207407) and Dheeraj (z5204820)
+ * 
+ * Started: 24/10/2019 | Last edited: 14/11/2019
+ * 
+ * Acknowledgement: some of the code may be similar to the lab code.
+ */
+
+
+package unsw.dungeon;
+
+import java.io.IOException;
+
+public class QuickMenuHandler {
+    public QuickMenuHandler(DungeonController dControl) throws IOException {
+        
+    }
+    
+}
